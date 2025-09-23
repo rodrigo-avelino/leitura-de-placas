8e2fd431a93f3f9988b326b8a246f8dae25e61ba
João Vitor Silva <joaovitorssilva007@gmail.com>
2025-09-23 20:56:51 -0300

    ajuste feito na view do consulta registro

--- src/pages/consultarRegistroPage.py
+++ src/pages/consultarRegistroPage.py
@@ -5,9 +5,15 @@ from src.controllers.placaController import PlacaController
 
 class consultarRegistroPage:
     def app():
-        st.title("📑 Consultar Registros")
+        st.title("Consultar Registros")
 
         filtros = filtros_busca()
-        registros = PlacaController.consultarRegistros(filtros)
+        registros = [
+            {"placa": "ABC-1234", "data": "12/09/2025 10:14:00", "imagem": "src/static/crops/crop_teste1.png"},
+            {"placa": "XYZ-5678", "data": "12/09/2025 10:20:00", "imagem": "src/static/crops/crop_teste1.png"},
+            {"placa": "JKL-9012", "data": "12/09/2025 10:25:00", "imagem": "src/static/crops/crop_teste1.png"},  # sem imagem
+        ]
+
+        #registros = PlacaController.consultarRegistros(filtros)
 
         registros_table(registros)
